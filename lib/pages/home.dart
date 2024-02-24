29277c883c65c91d973bb745d318aca5415e67e0
Jidnya J <101974857+suhracha@users.noreply.github.com>
2024-02-24 09:04:22 +0530

    Jidnya Update

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../widget/widget_support.dart';
@@ -20,26 +21,26 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.only(top: 50, left: 20, right: 20),
+        margin: EdgeInsets.only(top: 10, left: 20, right: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(Icons.person_outlined, color:Color(0xFFB784B7)),
-                Text("Hello jj",
-                  style: AppWidget.boldTextFieldStyle(),
-                ),
-                Container(
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(color: Color(0xFFB784B7),
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Icon(Icons.shopping_cart, color: Colors.white,),
-                ),
-              ],
-            ),
-            SizedBox(height: 20,),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            //   children: [
+            //   // Icon(Icons.person_outlined, color:Color(0xFFB784B7))),
+            //   //   Text("Hello jj",
+            //   //     style: AppWidget.boldTextFieldStyle(),
+            //   //   ),
+            //     Container(
+            //       padding: EdgeInsets.all(5),
+            //       decoration: BoxDecoration(color: Color(0xFFB784B7),
+            //           borderRadius: BorderRadius.circular(8)),
+            //       child: Icon(Icons.shopping_cart, color: Colors.white,),
+            //     ),
+            //   ],
+            // ),
+            SizedBox(height: 10,),
             Text("PillPal",
               style: AppWidget.headerTextFieldStyle(),
             ),
@@ -59,146 +60,154 @@ class _HomeState extends State<Home> {
   }
 
   Widget showItems() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        GestureDetector(
-          onTap: () {
-            balms = true;
-            tablets = false;
-            syrup = false;
-            essentials = false;
-            setState(() {
-
-            });
-          },
-          child: Material(
-            elevation: 5,
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: balms ? Color(0xFFB784B7) : Colors.white,
-                  borderRadius: BorderRadius.circular(10)),
-              padding: EdgeInsets.only(
-                  left: 12, top: 12, right: 12, bottom: 15),
-              // child: Image.asset("images/balm.png", height: 60, width: 60, fit: BoxFit.cover),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset("images/firstAid.png", height: 60,
-                      width: 60,
-                      fit: BoxFit.cover),
-                  SizedBox(height: 8),
-                  Text(
-                      "First Aid",
-                      style: AppWidget.categoryTextFieldStyle()
-                  ),
-                ],),
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          
+          GestureDetector(
+            onTap: () {
+              balms = true;
+              tablets = false;
+              syrup = false;
+              essentials = false;
+              setState(() {
+      
+              });
+            },
+            child: Material(
+              elevation: 5,
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: balms ? Color(0xFFB784B7) : Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.only(
+                    left: 12, top: 12, right: 12, bottom: 2),
+                // child: Image.asset("images/balm.png", height: 60, width: 60, fit: BoxFit.cover),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset("images/firstAid.png", height: 60,
+                        width: 70,
+                        fit: BoxFit.cover),
+                    SizedBox(height: 8, width:20,),
+                    Text(
+                        "First Aid",
+                        style: AppWidget.categoryTextFieldStyle()
+                    ),
+                  ],),
+              ),
             ),
           ),
-        ),
-        GestureDetector(
-          onTap: () {
-            balms = false;
-            tablets = true;
-            syrup = false;
-            essentials = false;
-            setState(() {
-
-            });
-          },
-          child: Material(
-            elevation: 5,
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: tablets ? Color(0xFFB784B7) : Colors.white,
-                  borderRadius: BorderRadius.circular(10)),
-              padding: EdgeInsets.only(
-                  left: 12, top: 12, right: 12, bottom: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset("images/medicine.png", height: 60,
-                      width: 60,
-                      fit: BoxFit.cover),
-                  SizedBox(height: 8),
-                  Text(
-                      "Tablets",
-                      style: AppWidget.categoryTextFieldStyle()
-                  ),
-                ],),
+          GestureDetector(
+            onTap: () {
+              balms = false;
+              tablets = true;
+              syrup = false;
+              essentials = false;
+              setState(() {
+      
+              });
+            },
+            child: Material(
+              elevation: 5,
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: tablets ? Color(0xFFB784B7) : Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.only(
+                    left: 12, top: 12, right: 12, bottom: 2),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset("images/medicine.png", height: 60,
+                        width: 70,
+                        fit: BoxFit.cover),
+                    SizedBox(height: 8, width: 20,),
+                    Text(
+                        "Tablets",
+                        style: AppWidget.categoryTextFieldStyle()
+                    ),
+                  ],),
+              ),
             ),
           ),
-        ),
-        GestureDetector(
-          onTap: () {
-            balms = false;
-            tablets = false;
-            syrup = true;
-            essentials = false;
-            setState(() {
-
-            });
-          },
-          child: Material(
-            elevation: 5,
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: syrup ? Color(0xFFB784B7) : Colors.white,
-                  borderRadius: BorderRadius.circular(10)),
-              padding: EdgeInsets.only(
-                  left: 12, top: 12, right: 12, bottom: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset("images/syrup2.png", height: 60,
-                      width: 60,
-                      fit: BoxFit.cover),
-                  SizedBox(height: 8),
-                  Text(
-                      "Syrups",
-                      style: AppWidget.categoryTextFieldStyle()
-                  ),
-                ],),
+          GestureDetector(
+            onTap: () {
+              balms = false;
+              tablets = false;
+              syrup = true;
+              essentials = false;
+              setState(() {
+      
+              });
+            },
+            child: Material(
+              elevation: 5,
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: syrup ? Color(0xFFB784B7) : Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.only(
+                    left: 12, top: 12, right: 12, bottom: 2),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset("images/syrup2.png", height: 60,
+                        width: 70,
+                        fit: BoxFit.cover),
+                    SizedBox(height: 8),
+                    Text(
+                        "Syrups",
+                        style: AppWidget.categoryTextFieldStyle()
+                    ),
+                  ],),
+              ),
             ),
           ),
-        ),
-        GestureDetector(
-          onTap: () {
-            balms = false;
-            tablets = false;
-            syrup = false;
-            essentials = true;
-            setState(() {
-
-            });
-          },
-          child: Material(
-            elevation: 5,
-            borderRadius: BorderRadius.circular(10),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: essentials ? Color(0xFFB784B7) : Colors.white,
-                  borderRadius: BorderRadius.circular(10)),
-              padding: EdgeInsets.only(
-                  left: 12, top: 12, right: 12, bottom: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.asset("images/essentials1.png", height: 60,
-                      width: 60,
-                      fit: BoxFit.cover),
-                  SizedBox(height: 8),
-                  Text(
-                      "Essentials",
-                      style: AppWidget.categoryTextFieldStyle()
-                  ),
-                ],),
+          GestureDetector(
+            onTap: () {
+              balms = false;
+              tablets = false;
+              syrup = false;
+              essentials = true;
+              setState(() {
+      
+              });
+            },
+            child: Material(
+              elevation: 5,
+              borderRadius: BorderRadius.circular(10),
+              child: Container(
+                margin: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                    color: essentials ? Color(0xFFB784B7) : Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.only(
+                    left: 12, top: 12, right: 12, bottom: 2),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.asset("images/essentials1.png", height: 60,
+                        width: 70,
+                        fit: BoxFit.cover),
+                    SizedBox(height: 10),
+                    Text(
+                        "Essentials",
+                        style: AppWidget.categoryTextFieldStyle()
+                    ),
+                  ],),
+              ),
             ),
-          ),
-        )
-      ],);
+          )
+        ],),
+    );
   }
 }
